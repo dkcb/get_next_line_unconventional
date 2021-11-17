06ec474717f863cbcbecebfdcdedc93e929a9984
Danila Kocob <dkocob@f1r1s7.codam.nl>
2021-11-17 21:56:19 +0100

    ready

---
+++ get_next_line.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dkocob <dkocob@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/04 15:57:56 by dkocob        #+#    #+#                 */
+/*   Updated: 2021/11/17 21:20:15 by dkocob        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+size_t	ft_strlcpy(char *d, char *s, int len)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && i < len)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	d[i] = '\0';
+	return (i);
+}
+
+size_t	dc(char *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
+char	*gnl(int fd, char *line, int i, int pos)
+{
+	static char	re[BUFFER_SIZE + 1];
+
+	if (dc(re, '\n') != dc(re, '\0'))
+	{
+		ft_strlcpy(line, re, dc(re, '\n'));
+		ft_strlcpy(re, &re[dc(re, '\n') + 1], dc(&re[dc(re, '\n')], '\0'));
+		return (line);
+	}
+	if (re[0] != '\0')
+	{
+		pos += dc(re, '\0');
+		ft_strlcpy(line, re, pos);
+		re[0] = '\0';
+	}
+	while (i && dc(re, '\n') == dc(re, '\0'))
+	{
+		i = read(fd, re, BUFFER_SIZE);
+		ft_strlcpy(&line[pos], re, dc(re, '\n'));
+		pos += i;
+	}
+	if (i == 0 && re[0] == '\0' && line[0] == '\0')
+		return (NULL);
+	ft_strlcpy(re, &re[dc(re, '\n') + 1], dc(&re[dc(re, '\n')], '\0'));
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*line;
+
+	line = malloc(sizeof(char) * 200000);
+	if (!line)
+		return (NULL);
+	return (gnl(fd, line, 1, 0));
+}
---
+++ main_gnl.c
@@ -0,0 +1,34 @@
+#include "get_next_line.h"
+// #include "get_next_line_utils.c"
+#include "get_next_line.c"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+	int fd;
+	int fd2;
+	char *yo = "1234";
+	char *ww;
+	int i = 1;
+	int j = 0;
+	int l = 0;
+
+	if ((fd = open ("./empty_lines.txt", O_CREAT | O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO)) > 0)
+	{
+		while(i < 15)
+		{
+			ww = get_next_line(fd);
+			if (ww == NULL)
+				break ;
+			printf("%d_ret_fd(%d)_Line_%d= -|%s|-\n",i, fd, l + 1, ww);
+			l++;
+
+			free(ww);
+			i++;
+		}
+	}
+printf("------- closed1(%d) ------ ", close(fd));
+}
